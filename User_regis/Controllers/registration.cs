2a26beaceb190977994ab911d8ffe0c2062dd370
Ashish Tripathi <Ashish Tripathi@DESKTOP-TK6QOTK>
2021-12-03 11:09:21 +0530

    Add project files.

---
+++ User_regis/Controllers/registration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using User_regis.Models;
+
+
+namespace User_regis.Controllers
+{
+    public class registration : Controller
+    {
+        private readonly ApplicationContext _auc;
+
+        public registration(ApplicationContext auc)
+        {
+            _auc = auc;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(User_Model uc)
+        {
+            _auc.Add(uc);
+            _auc.SaveChanges();
+            ViewBag.message = "The user " + uc.Username + " is saved successfully";
+            return View();
+        }
+    }
+}
